d92276f38373484872be720c416f4137b88cac1f
Ilya Shargorodskiy <isharg@nb-vcjql6wp0g.barn-sun.ts.net>
2024-05-14 17:14:04 +0200

    fix date detection

--- vfs_appointment_bot/utils/date_utils.py
+++ vfs_appointment_bot/utils/date_utils.py
@@ -2,7 +2,7 @@ import re
 
 
 def extract_date_from_string(text):
-    pattern = r"(\d{4}-\d{2}-\d{2}|\d{2}-\d{2}-\d{4}|\d{2}-\d{2}-\d{2})"
+    pattern = r"(\d{4}-\d{2}-\d{2}|\d{2}-\d{2}-\d{4}|\d{2}-\d{2}-\d{2}|\d{2}/\d{2}/\d{4})"
     match = re.search(pattern, text)
     if match:
         return match.group()
